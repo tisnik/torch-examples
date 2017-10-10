f43b7f675a3c6552505cb9e9e5e9c015df42542e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-11 01:18:20 +0200

    Constructor torch.zeros()

---
+++ basics/02_zeros_constructor.lua
@@ -0,0 +1,17 @@
+x1 = torch.Tensor(10):zero()
+print(x1)
+
+x2 = torch.Tensor(4,4):zero()
+print(x2)
+
+z0 = torch.zeros(1)
+print(z0)
+
+z1 = torch.zeros(10)
+print(z1)
+
+z2 = torch.zeros(3,4)
+print(z2)
+
+z3 = torch.zeros(2,3,4)
+print(z3)

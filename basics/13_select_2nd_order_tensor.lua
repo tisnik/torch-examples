799629e3fdc912b6a354eb12b8960605e0b57115
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-17 00:12:50 +0200

    Usage of select operation for 2D matrix

---
+++ basics/13_select_2nd_order_tensor.lua
@@ -0,0 +1,22 @@
+x = torch.Tensor(5,5)
+
+s = x:storage()
+
+for i = 1,s:size() do
+    s[i]=i
+end
+
+print(x)
+
+print(x:select(1, 1))
+print(x:select(1, 5))
+print(x:select(2, 1))
+print(x:select(2, 5))
+
+print(x[1])
+print(x[5])
+
+print(x:isContiguous())
+print(x:select(1,1):isContiguous())
+print(x:select(2,1):isContiguous())
+print(x[1]:isContiguous())

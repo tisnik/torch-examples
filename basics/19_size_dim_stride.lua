55c0a4137d981ebd683cea2a0c790a5697689f2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-18 00:02:04 +0200

    Display size, dimensions, and stride

---
+++ basics/19_size_dim_stride.lua
@@ -0,0 +1,26 @@
+function printTensorInfo(tensor)
+    print("Size:")
+    print(tensor:size())
+    print("Elements:")
+    print(tensor:nElement())
+    print("Dimensions:")
+    print(tensor:dim())
+    print("Stride:")
+    print(tensor:stride())
+    print("Storage offset:")
+    print(tensor:storageOffset())
+end
+
+v = torch.range(1, 10)
+m = torch.Tensor(5, 6)
+t3 = torch.Tensor(4, 5, 6)
+t4 = torch.Tensor(2, 3, 4, 5)
+
+printTensorInfo(v)
+printTensorInfo(m)
+printTensorInfo(t3)
+printTensorInfo(t4)
+
+printTensorInfo(v[{ {5,8} }])
+printTensorInfo(m[{ {1,1},{1,1} }])
+printTensorInfo(m[{ {2,2},{2,2} }])

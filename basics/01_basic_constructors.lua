8d66a0fccdb2d80a6ac42622b8c5b3b44fd998bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-24 10:35:00 +0200

    Added license

--- basics/01_basic_constructors.lua
+++ basics/01_basic_constructors.lua
@@ -1,3 +1,15 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
 s1 = torch.Tensor(1)
 s1[1] = 42
 print(s1)

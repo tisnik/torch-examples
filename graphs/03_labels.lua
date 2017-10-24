0e04c3afffafd1823071bd3697b46aae062323ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-24 10:33:44 +0200

    Graph with explicitly set labels

---
+++ graphs/03_labels.lua
@@ -0,0 +1,23 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+require("gnuplot")
+
+x = torch.range(1, 360, 5)
+y = torch.sin(x/180.0*math.pi)
+
+gnuplot.pngfigure("plot3.png")
+gnuplot.title("sin x")
+gnuplot.xlabel("x")
+gnuplot.ylabel("sin x")
+gnuplot.plot(x, y)
+gnuplot.plotflush()

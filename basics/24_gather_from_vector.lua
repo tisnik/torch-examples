1bd07dcc2264fd75222cd1a1a055124f211baa97
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-24 10:34:28 +0200

    Updated - use gather() on vector

--- basics/24_gather_from_vector.lua
+++ basics/24_gather_from_vector.lua
@@ -1,3 +1,15 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
 v1 = torch.range(10, 100, 10)
 print(v1)
 
@@ -6,18 +18,13 @@ print(indexes)
 
 print(v1:gather(1, indexes))
 
-v = torch.range(1, 24)
-m1 = v:resize(6, 4)
-print(m1)
+print(v1:gather(1, indexes):isContiguous())
 
-indexes = torch.LongTensor({{1,1,1,1}, {2,2,2,2}, {3,3,3,3}})
-print(indexes)
-print(m1:gather(1, indexes))
+print("-----------------------")
 
-indexes = torch.LongTensor({{1,2,1,2}, {2,1,2,1}})
+indexes = torch.range(10, 1, -1):long()
 print(indexes)
-print(m1:gather(1, indexes))
 
-indexes = torch.LongTensor({{6,6,6,6}, {5,5,5,5}, {4,4,4,4}, {3,3,3,3}, {2,2,2,2}, {1,1,1,1}})
-print(indexes)
-print(m1:gather(1, indexes))
+print(v1:gather(1, indexes))
+
+print(v1:gather(1, indexes):isContiguous())

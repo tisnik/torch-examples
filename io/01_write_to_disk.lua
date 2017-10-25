e120867fbc202b34ec9eea7abedb874d6ef3e590
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-25 09:27:12 +0200

    Serialize tensors into both ASCII and binary formats

--- io/01_write_to_disk.lua
+++ io/01_write_to_disk.lua
@@ -11,7 +11,11 @@
 --
 
 function writeTensor(filename, tensor)
-    fout = torch.DiskFile(filename, "w")
+    local fout = torch.DiskFile(filename .. ".asc", "w"):ascii()
+    fout:writeObject(tensor)
+    fout:close()
+
+    local fout = torch.DiskFile(filename .. ".bin", "w"):binary()
     fout:writeObject(tensor)
     fout:close()
 end
@@ -19,28 +23,28 @@ end
 s1 = torch.Tensor(1)
 s1[1] = 42
 print(s1)
-writeTensor("scalar_1.asc", s1)
+writeTensor("scalar_1", s1)
 
 s2 = torch.Tensor({42})
 print(s2)
-writeTensor("scalar_2.asc", s2)
+writeTensor("scalar_2", s2)
 
 v1 = torch.Tensor(3)
 v1[1] = 10
 v1[2] = 20
 v1[3] = 30
 print(v1)
-writeTensor("vector1.asc", v1)
+writeTensor("vector1", v1)
 
 v2 = torch.Tensor({10,20,30})
 print(v2)
-writeTensor("vector2.asc", v2)
+writeTensor("vector2", v2)
 
 m = torch.Tensor({{10,20,30}, {40,50,60}, {70,80,90}})
 print(m2)
-writeTensor("matrix.asc", m)
+writeTensor("matrix", m)
 
 v = torch.range(1, 24)
 q = v:resize(3, 2, 2, 2)
 print(q)
-writeTensor("tensor_3.asc", q)
+writeTensor("tensor_3", q)

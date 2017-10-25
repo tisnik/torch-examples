fe08dbd7615bb3e14ccbbaf7106fc1f000345176
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-25 09:19:53 +0200

    Plotting bars

---
+++ graphs/07_bars.lua
@@ -0,0 +1,22 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+require("gnuplot")
+
+x = torch.range(0, 360, 10)
+y = torch.sin(x/180.0*math.pi)
+
+gnuplot.pngfigure("plot6.png")
+gnuplot.title("sin x")
+gnuplot.bar(x, y)
+gnuplot.plotflush()
+gnuplot.close()

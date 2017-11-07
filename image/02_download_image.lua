229c1e1a448a8eb9569ddb61d63c0b7b10b37560
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-07 09:27:14 +0100

    Download image from external source

---
+++ image/02_download_image.lua
@@ -0,0 +1,32 @@
+require("image")
+
+
+original_image_address = "https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"
+image_name = "lenna.png"
+
+
+function file_exists(filename)
+    local fin = io.open(filename,"r")
+    if fin then
+        io.close(fin)
+        return true
+    else
+        return false
+    end
+end
+
+
+function download_file(address, filename)
+    local command = "wget -v -O " .. filename .. " " .. address
+    os.execute(command)
+end
+
+
+function setup(address, filename)
+    if not file_exists(filename) then
+        download_file(original_image_address, filename)
+    end
+end
+
+
+setup(original_image_address, image_name)

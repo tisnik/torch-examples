4a652a909b8c102c393f396d5553d964ad13b5c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-10 12:03:59 +0100

    Gaussian pyramid

---
+++ image/13_gaussian_pyramid.lua
@@ -0,0 +1,53 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+require("image")
+
+
+original_image_address = "https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"
+image_name = "lenna.png"
+
+
+function file_exists(filename)
+    local fin = io.open(filename,"r")
+    if fin then
+        io.close(fin)
+        return true
+    else
+        return false
+    end
+end
+
+
+function download_file(address, filename)
+    local command = "wget -v -O " .. filename .. " " .. address
+    os.execute(command)
+end
+
+
+function setup(address, filename)
+    if not file_exists(filename) then
+        download_file(original_image_address, filename)
+    end
+end
+
+
+setup(original_image_address, image_name)
+
+lenna = image.load(image_name)
+
+pyramid = image.gaussianpyramid(lenna, {1/2, 1/4, 1/8, 1/16})
+
+for i, img in ipairs(pyramid) do
+    image.save("pyramid_level" .. i .. ".png", img)
+end
+

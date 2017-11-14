2189f9b2b788fd950765ca47fcbdabc41c0e5464
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-14 14:41:51 +0100

    LCN filter

---
+++ image/17_default_lcn.lua
@@ -0,0 +1,62 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+require("image")
+require("gnuplot")
+
+
+original_image_address = "https://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"
+image_name = "lenna.png"
+
+
+function file_exists(filename)
+    local fin = io.open(filename,"r")
+    if fin then
+        io.close(fin)
+        return true
+    else
+        return false
+    end
+end
+
+
+function file_exists(filename)
+    local fin = io.open(filename,"r")
+    if fin then
+        io.close(fin)
+        return true
+    else
+        return false
+    end
+end
+
+
+function download_file(address, filename)
+    local command = "wget -v -O " .. filename .. " " .. address
+    os.execute(command)
+end
+
+
+function setup(address, filename)
+    if not file_exists(filename) then
+        download_file(original_image_address, filename)
+    end
+end
+
+
+setup(original_image_address, image_name)
+
+lenna = image.load(image_name, 1)
+image.save("lenna_grayscale.png", lenna)
+
+lcn_default = image.lcn(lenna)
+image.save("lcn_default.png", lcn_default)

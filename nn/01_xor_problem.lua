1cda72617b8832d1c48ea28884b19e473f612525
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-22 09:30:37 +0100

    Test/validate

--- nn/01_xor_problem.lua
+++ nn/01_xor_problem.lua
@@ -52,8 +52,8 @@ function train_neural_network(network, training_data, learning_rate, max_iterati
 end
 
 
-function test_neural_network(network, test_data)
-    for i,d in ipairs(test_data) do
+function validate_neural_network(network, validation_data)
+    for i,d in ipairs(validation_data) do
         d1, d2 = d[1], d[2]
         input = torch.Tensor({d1, d2})
         prediction = network:forward(input)[1]
@@ -75,7 +75,7 @@ x=torch.Tensor({0.5, -0.5})
 prediction = network:forward(x)
 print(prediction)
 
-test_data = {
+validation_data = {
     { 1.0,  1,0},
     { 0.5,  0.5},
     { 0.2,  0.2},
@@ -93,5 +93,5 @@ test_data = {
     {-0.2, -0.2},
 }
 
-test_neural_network(network, test_data)
+validate_neural_network(network, validation_data)
 

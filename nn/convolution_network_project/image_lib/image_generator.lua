9a01978a9278d60cee76d1ec02281e4f2f00cd2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-21 00:05:53 +0100

    Jittered image filter

--- nn/convolution_network_project/image_lib/image_generator.lua
+++ nn/convolution_network_project/image_lib/image_generator.lua
@@ -33,6 +33,23 @@ function generate_noisy_image_for_validation(digit, scale, noise_variance, black
     return validation_image
 end
 
+
+function jitter_image_filename(digit, jitter_variance)
+    return string.format("validation_%d_jitter_%d.pgm", digit, jitter_variance)
+end
+
+
+function generate_jittered_image_for_validation(digit, scale, jitter_variance, black_level, white_level, export_image)
+    local validation_image = generate_image(digit, scale, black_level, white_level)
+    validation_image = apply_jitter(validation_image, jitter_variance)
+    if export_image then
+        local filename = jitter_image_filename(digit, jitter_variance)
+        write_image(filename, validation_image)
+    end
+    return validation_image
+end
+
+
 function image2tensor(image)
     -- image is 2d table: height x width
     -- but we need 3d table: 1 x height x width

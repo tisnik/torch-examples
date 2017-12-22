02a0d481a89f8746cc99aecb682ebcd956caea51
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-22 10:43:05 +0100

    Comments to tensor constructors

--- basics/01_basic_constructors.lua
+++ basics/01_basic_constructors.lua
@@ -10,28 +10,42 @@
 --      Pavel Tisnovsky
 --
 
+
+-- první varianta kontrukce tenzoru
 s1 = torch.Tensor(1)
 s1[1] = 42
 print(s1)
 
+
+-- druhá varianta konstrukce tenzoru
 s2 = torch.Tensor({42})
 print(s2)
 
+
+-- tenzor ve funkci vektoru se třemi prvky
 v1 = torch.Tensor(3)
 v1[1] = 10
 v1[2] = 20
 v1[3] = 30
 print(v1)
 
+
+-- alternativní konstrukce z tabulky
 v2 = torch.Tensor({10,20,30})
 print(v2)
 
+
+-- tenzor ve funkci matice
 m1 = torch.Tensor(3,3)
 print(m1)
 
+
+-- alternativní konstrukce z tabulky
 m2 = torch.Tensor({{1,2,3}, {4,5,6}, {7,8,9}})
 print(m2)
 
+
+-- čtyřrozměrná struktura
 q = torch.Tensor({{{1,2,3}, {4,5,6}, {7,8,9}},
                   {{9,8,7}, {6,5,4}, {3,2,1}}})
 print(q)

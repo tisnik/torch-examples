173431d52537ce13dd7f7e2f837e434f98a43a1e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-22 10:44:19 +0100

    Added license

--- nn/convolution_network_project/image_lib/image_filters.lua
+++ nn/convolution_network_project/image_lib/image_filters.lua
@@ -1,3 +1,16 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+
 function gaussian(mean, variance)
     return math.sqrt(-2 * variance * math.log(math.random())) *
            math.cos(2 * variance * math.pi * math.random()) + mean

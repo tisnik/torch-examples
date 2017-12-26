3cba5c1389c26f213f6695dcf5ea78b682536105
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-26 21:30:41 +0100

    Added license

--- nn/convolution_network_project/image_lib/image_renderer.lua
+++ nn/convolution_network_project/image_lib/image_renderer.lua
@@ -1,3 +1,16 @@
+--
+--  (C) Copyright 2017  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
+
 digits = {
     {0x00, 0x3C, 0x66, 0x76, 0x6E, 0x66, 0x3C, 0x00 },
     {0x00, 0x18, 0x1C, 0x18, 0x18, 0x18, 0x7E, 0x00 },

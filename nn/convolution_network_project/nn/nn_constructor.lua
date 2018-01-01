b075d8a2c42b14dc4a0fcc420f5b43e2b5339262
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-01 11:28:32 +0100

    Added license

--- nn/convolution_network_project/nn/nn_constructor.lua
+++ nn/convolution_network_project/nn/nn_constructor.lua
@@ -1,3 +1,15 @@
+--
+--  (C) Copyright 2017, 2018  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
 function calculate_size_after_convolution(input_size, middle_planes, convolution_kernel_size)
     local size = input_size
     for i=1,#middle_planes do

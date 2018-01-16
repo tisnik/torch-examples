4481aee83cfb4822ba6c59baf8094ba602f3177c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-16 15:00:49 +0100

    Added license

--- nn/cifar_data_classificator2/nn/nn_validators.lua
+++ nn/cifar_data_classificator2/nn/nn_validators.lua
@@ -1,3 +1,15 @@
+--
+--  (C) Copyright 2018  Pavel Tisnovsky
+--
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+--
+--  Contributors:
+--      Pavel Tisnovsky
+--
+
 function find_largest_item(tensor)
     local index = -1
     local value = -math.huge
